e99192fd6b61b3ad3dc0e251b279480eaac9f9b2
yeo myoung Kim <kdjwand@gmail.com>
2026-02-23 15:16:41 +0900

    Add ray recursion depth and hemisphere bounces
    카메라에 maxDepth를 도입하고 이를 RayColor에 스레딩하여 재귀 광선 추적을 제한하고 무한 재귀를 방지한다.
    
    일반 기반 색상을 반구 기반 샘플링(RandomOnHemisphere)으로 교체하여 간접 조명을 구현

--- vec3.h
+++ vec3.h
@@ -63,32 +63,6 @@ public:
         );
     }
 
-    inline Vec3 RandomUnitVector()
-    {
-        while (true)
-        {
-            auto p = Vec3::Random(-1.0, 1.0);
-            auto lengthSquared = p.LengthSquared();
-    
-            if (1e-160 < lengthSquared && lengthSquared <= 1.0)
-            {
-                return p / std::sqrt(lengthSquared);
-            }
-        }
-    }
-
-    inline Vector3 RandomOnHemisphere(const Vector3& normal)
-    {
-        Vector3 unitSphereDirection = RandomUnitVector();
-    
-        // In the same hemisphere as the normal
-        if (Dot(unitSphereDirection, normal) > 0.0) 
-        {
-            return unitSphereDirection;
-        }
-    
-        return -unitSphereDirection;
-    }
 public:
     double E[3];
 };
@@ -149,4 +123,31 @@ inline Vector3 Cross(const Vector3& u, const Vector3& v)
 inline Vector3 UnitVector(const Vector3& v)
 {
     return v / v.Length();
+}
+
+inline Vec3 RandomUnitVector()
+{
+    while (true)
+    {
+        auto p = Vec3::Random(-1.0, 1.0);
+        auto lengthSquared = p.LengthSquared();
+
+        if (1e-160 < lengthSquared && lengthSquared <= 1.0)
+        {
+            return p / std::sqrt(lengthSquared);
+        }
+    }
+}
+
+inline Vector3 RandomOnHemisphere(const Vector3& normal)
+{
+    Vector3 unitSphereDirection = RandomUnitVector();
+
+    // In the same hemisphere as the normal
+    if (Dot(unitSphereDirection, normal) > 0.0)
+    {
+        return unitSphereDirection;
+    }
+
+    return -unitSphereDirection;
 }
