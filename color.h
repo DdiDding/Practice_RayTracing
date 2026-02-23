514827fe9c60eeee94fedddfa3f3808b03a4b4f6
yeo myoung Kim <kdjwand@gmail.com>
2026-02-23 15:47:05 +0900

    Apply gamma correction before color output
    LinearToGamma 도우미(sqrt)를 추가하고 음수를 0.0으로 설정하여,
    각 RGB 구성 요소에 대해 gamma=2.0 보정을 수행

--- color.h
+++ color.h
@@ -5,6 +5,19 @@
 
 using Color = Vector3;
 
+/**
+ * 감마 보정을 위해 선형 공간에서 감마 공간으로 이동, 감마2의 역을 취한다.
+ */
+inline double LinearToGamma(double linearComponent)
+{
+    if (linearComponent > 0.0)
+    {
+        return std::sqrt(linearComponent);
+    }
+
+    return 0.0;
+}
+
 // vec3클래스를 사용하여 단일 색상을 기록하는 함수
 void WriteColor(std::ostream& out, const Color& pixelColor)
 {
@@ -12,16 +25,22 @@ void WriteColor(std::ostream& out, const Color& pixelColor)
     auto green = pixelColor.Y();
     auto blue = pixelColor.Z();
 
+    // 감마 2.0공간으로 이동
+    red = LinearToGamma(red);
+    green = LinearToGamma(green);
+    blue = LinearToGamma(blue);
+
+
+    // [0,1] 범위의 컴포넌트 값을 바이트 범위 [0,255]로 변환
     static const Interval intensity(0.000, 0.999);
 
-    double cRed = intensity.Clamp(red);
-    double cGreen = intensity.Clamp(green);
-    double cBlue = intensity.Clamp(blue);
-
-    // [0,1] 범위의 컴포넌트 값을 바이트 범위 [0,255]로 변환합니다.
-    int redByte = static_cast<int>(256.0 * intensity.Clamp(cRed));
-    int greenByte = static_cast<int>(256.0 * intensity.Clamp(cGreen));
-    int blueByte = static_cast<int>(256.0 * intensity.Clamp(cBlue));
+    red = intensity.Clamp(red);
+    green = intensity.Clamp(green);
+    blue = intensity.Clamp(blue);
+    
+    int redByte = static_cast<int>(256.0 * red);
+    int greenByte = static_cast<int>(256.0 * green);
+    int blueByte = static_cast<int>(256.0 * blue);
 
     // 픽셀 색상 컴포넌트를 출력합니다.
     out << redByte << ' ' << greenByte << ' ' << blueByte << '\n';
