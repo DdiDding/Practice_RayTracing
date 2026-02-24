22a90f4f8565383269e0ab4f1482cf6509f42154
yeo myoung Kim <kdjwand@gmail.com>
2026-02-24 13:41:58 +0900

    Add Dielectric material and fix background colors

--- material.h
+++ material.h
@@ -65,3 +65,35 @@ private:
     Color mAlbedo;
     double mFuzz;
 };
+
+class Dielectric : public Material
+{
+public:
+    explicit Dielectric(double refractionIndex)
+        : mRefractionIndex(refractionIndex) { }
+    
+
+    bool Scatter(
+        const Ray& rayIn,
+        const HitRecord& hitRecord,
+        Color& attenuation,
+        Ray& scattered
+    ) const override
+    {
+        attenuation = Color(1.0, 1.0, 1.0);
+
+        const double etaInOverEtaOut =
+            hitRecord.bFrontFace ? (1.0 / mRefractionIndex) : mRefractionIndex;
+
+        const Vec3 unitDirection = UnitVector(rayIn.Direction());
+        const Vec3 refracted = Refract(unitDirection, hitRecord.Normal, etaInOverEtaOut);
+
+        scattered = Ray(hitRecord.Point, refracted);
+
+        return true;
+    }
+
+private:
+    // Refraction index (IOR). For air/vacuum -> material, typical  glass is ~1.5.
+    double mRefractionIndex = 1.0;
+};
